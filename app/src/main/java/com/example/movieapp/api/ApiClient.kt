8f956e8137e12223b473a3392c33677f0d4eba90
aayuk <aayudhawara@gmail.com>
2022-07-23 11:08:34 +0700

    first commit

---
+++ app/src/main/java/com/example/movieapp/api/ApiClient.kt
@@ -0,0 +1,19 @@
+package com.project.movieapp.api
+
+import com.example.movieapp.BuildConfig
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private val httpClient = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .client(httpClient)
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+
+    val instance: ApiService = retrofit.build().create(ApiService::class.java)
+
+}
